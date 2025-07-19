83d88181a46c33007b741581a4fdc70e547a6f6e
Alan Maizon <alanmaizon@hotmail.com>
2025-07-19 20:23:01 +0100

    CHANGES

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  // 👇 Copy _redirects manually on build
+  closeBundle() {
+    copyFileSync('public/_redirects', 'dist/_redirects')
+  },
 })
